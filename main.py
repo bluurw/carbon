f70aa727b61a9fa8144e2247cf220b3401f1b939
Maserati <maserati@localhost.localdomain>
2025-04-02 20:50:41 -0400

    mudancas na execucao, e inclusao de banner e painel

--- main.py
+++ main.py
@@ -1,66 +1,79 @@
-from telethon import TelegramClient
-from telethon.tl.functions.channels import JoinChannelRequest, LeaveChannelRequest
+from carbon import Carbon
+import os
 
+import banners
+import panels
+
+# Criar um arquivo para guardar estas configuracoes
 API_ID =   #API ID
 API_HASH = "" #API Hash
+Carbon = Carbon(API_ID, API_HASH)
 
-client = TelegramClient("session_name", API_ID, API_HASH)
+while True:
+    banners.carbon_banner_bluured()
+    panels.options_panel()
+    choose_option = int(input('Enter chosen option: '))
+    
+    if choose_option == 1:
+        
+        banners.carbon_banner_bluured()
+        panels.options_clone_panel()
+        choose_option_save_clone = int(input('Enter chosen option: '))
+        
+        if choose_option_save_clone == 1:
+            banners.carbon_banner_bluured()
+            print('\033[1mC\033[0mLONAR_\033[1mC\033[0mANAL - SAVE_FOWARD')
+            source_input = str(input(f'INSIRA O NOME DO GRUPO ORIGEM: t.me/')) # INSIRA SOMENTE O NOME t.me/Nome
+            destination_input = str(input(f'INSIRA O NOME DO GRUPO DESTINO: t.me/')) # INSIRA SOMENTE O NOME t.me/Nome
+            SOURCE = f"t.me/{source_input}" 
+            DESTINATION = f"t.me/{destination_input}"
 
-# join group
-async def join_group(group_link):
-    await client.start()
-    try:
-        await client(JoinChannelRequest(group_link))
-        print(f"Successfully entered: {group_link}")
-    except Exception as e:
-        print(f"Failed to enter: {e}")
+            panels.filter_itens_panel()
+            filter_itens_input = list(input('Insira o numero de cada opcao que deseja filtrar: '))
+            filters = ["messages", "images", "videos", "audios", "docs", "links"]
+            filter = [filters[int(i)] for i in filter_itens_input]
 
+            Carbon.client.loop.run_until_complete(Carbon.toFoward(SOURCE, DESTINATION, filter=filter))
+            
+        elif choose_option_save_clone == 2:
+            banners.carbon_banner_bluured()
+            print('\033[1mC\033[0mLONAR_\033[1mC\033[0mANAL - SAVE_LOCAL')
+            source_input = str(input(f'INSIRA O NOME DO GRUPO ORIGEM: t.me/'))
+            SOURCE = f"t.me/{source_input}" # INSIRA SOMENTE O NOME t.me/Nome
+                
+            panels.filter_itens_panel()
+            filter_itens_input = list(input('Insira o numero de cada opcao que deseja filtrar: '))
+            filters = ["messages", "images", "videos", "audios", "docs", "links"]
+            filter = [filters[int(i)] for i in filter_itens_input]
 
-# source group = mandatory
-# destination group = mandatory
-# media = optional
-async def copy_messages(source, destination, include_media=True):
-    await client.start()
+            Carbon.client.loop.run_until_complete(Carbon.toLocalSave(SOURCE, filter=filter))
+            
+        elif choose_option_save_clone == 3:
+            print('Ainda em desenvolvimento')
+            
+        elif choose_option_save_clone == 0:
+            break
+            
+        else:
+            banners.carbon_banner_bluured()
+            input_clone_save_foward_panel()
 
-    try:
-        source_entity = await client.get_entity(source)
-        messages = await client.get_messages(source_entity, limit=100) #limited 100
-
-        for message in messages:
-            if message.media and include_media:
-                await client.send_file(destination, message.media, caption=message.text)
-            elif not message.media:
-                await client.send_message(destination, message.text)
-        print(f"Msgs copied from {source} to {destination}.")
-    except Exception as e:
-        print(f"Failed to copy msgs: {e}")
-
-# leave group
-async def leave_group(group_link):
-    await client.start()
-    try:
-        entity = await client.get_entity(group_link)
-        await client(LeaveChannelRequest(entity))
-        print(f"left: {group_link}")
-    except Exception as e:
-        print(f"failure to leave: {e}")
-
-
-
-
-with client:
-    SOURCE = "https://t.me/GROUP"
-    DESTINATION = "https://t.me/GROUP"
-
-    client.loop.run_until_complete(join_group(SOURCE))
-    client.loop.run_until_complete(copy_messages(SOURCE, DESTINATION, include_media=True))
-    client.loop.run_until_complete(leave_group(SOURCE))
-
-
-
-
-
-
+    elif choose_option == 2:
+        print('Arquivo de configuracao')
+        #Remover arquivo existente
+        #Criar novo arquivo
+    
+    elif choose_option == 3:
+        banners.carbon_banner_bluured()
+        about_me()
+        input()
+    
+    elif choose_option == 0:
+        os.system('exit')
+   
+    else:
+        banners.carbon_banner_bluured()
+        options_panel()
 
 
 # +5511998765432
