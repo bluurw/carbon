e4b42f07b4cf9805f5a8d593fb03e1b601757d25
Maserati <maserati@localhost.localdomain>
2025-04-02 20:51:02 -0400

    paineis

---
+++ panels.py
@@ -0,0 +1,42 @@
+def options_panel():
+    print('''
+[1] - \033[1mC\033[0mLONAR_\033[1mC\033[0mANAL
+[2] - OPTIONS
+[3] - ABOUT_\033[1mME\033[0m
+[0] - EXIT
+''')
+
+def options_clone_panel():
+    print('\033[1mC\033[0mLONAR_\033[1mC\033[0mANAL')
+    print('''
+[1] - CLONE_SAVE_FOWARD
+[2] - SAVE_LOCAL
+[3] - CLONE_SAVE_GITHUB
+[0] - RETURN
+''')
+
+def input_clone_save_foward_panel():
+    print('\033[1mC\033[0mLONAR_\033[1mC\033[0mANAL')
+    print('''
+[1] - SAVE_FOWARD
+[2] - SAVE_LOCAL
+[3] - SAVE_GITHUB
+[0] - RETURN
+''')
+
+def filter_itens_panel():
+    print('\033[1mC\033[0mLONAR_\033[1mC\033[0mANAL')
+    print('''
+[0] - MESSAGES
+[1] - IMAGES
+[2] - VIDEOS
+[3] - AUDIO
+[4] - DOCS
+[5] - LINKS
+''')
+
+def about_me():
+    print('The macaws above my head dont let me think.')
+    print('~telegram archive')
+    print('Github: https://github.com/bluurw')
+    print()
