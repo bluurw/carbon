51a5ce0d10622cd8bf49cf79c682fd28ddf8e2e0
Maserati <maserati@localhost.localdomain>
2025-04-04 10:35:56 -0400

    Atualizacao da interface e melhoramento do codigo

--- main.py
+++ main.py
@@ -6,8 +6,12 @@ import functions
 import banners
 import panels
 
-credentials = functions.load_credentials()
-Carbon = Carbon(credentials[0], credentials[1])
+def error_credentials():
+    banners.carbon_banner_bluured()
+    print('> The parakeets invalidate your key.')
+    print('> Create valid credencials, or correct existing ones.')
+    time.sleep(1.0)
+    input('\n...PRESS ANY KEY...')
 
 while True:
     #IMPLANTAR TRY
@@ -23,39 +27,61 @@ while True:
             choose_option_save_clone = int(input('> Enter chosen option: '))
             
             if choose_option_save_clone == 1:
-                banners.carbon_banner_bluured()
-                print('\033[1mC\033[0mLONAR_\033[1mC\033[0mANAL - SAVE_FOWARD \n')
-                source_input = str(input(f'> Source Group: t.me/'))
-                destination_input = str(input(f'> Destination Group: t.me/'))
-                SOURCE = f"t.me/{source_input}" 
-                DESTINATION = f"t.me/{destination_input}"
 
-                panels.filter_itens_panel()
-                filter_itens_input = list(input('> Insira o numero de cada opcao que deseja filtrar: '))
-                filters = ["messages", "images", "videos", "audios", "docs", "links"]
-                filter = [filters[int(i)] for i in filter_itens_input]
+                credentials = functions.load_credentials()
+            
+                if credentials == 0:
+                    error_credentials()
 
-                Carbon.client.loop.run_until_complete(Carbon.toFoward(SOURCE, DESTINATION, filter=filter))
+                else:
+                    banners.carbon_banner_bluured()
+                    print('\033[1mC\033[0mLONAR_\033[1mC\033[0mANAL - SAVE_FOWARD \n')
+                    source_input = str(input(f'> Source Group: t.me/'))
+                    destination_input = str(input(f'> Destination Group: t.me/'))
+                    SOURCE = f"t.me/{source_input}" 
+                    DESTINATION = f"t.me/{destination_input}"
+
+                    panels.filter_itens_panel()
+                    filter_itens_input = list(input('> Insira o numero de cada opcao que deseja filtrar: '))
+                    filters = ["messages", "images", "videos", "audios", "docs", "links"]
+                    filter = [filters[int(i)] for i in filter_itens_input]
+
+                    Carbon = Carbon(credentials[0], credentials[1])
+                    Carbon.client.loop.run_until_complete(Carbon.toFoward(SOURCE, DESTINATION, filter=filter))
                 
             elif choose_option_save_clone == 2:
-                banners.carbon_banner_bluured()
-                print('\033[1mC\033[0mLONAR_\033[1mC\033[0mANAL - SAVE_LOCAL \n')
-                source_input = str(input(f'> Source Group: t.me/'))
-                path_save_input = str(input(f'> Local Save: '))
-                SOURCE = f"t.me/{source_input}" # INSIRA SOMENTE O NOME t.me/Nome
-                    
-                panels.filter_itens_panel()
-                filter_itens_input = list(input('> Insira o numero de cada opcao que deseja filtrar: '))
-                filters = ["messages", "images", "videos", "audios", "docs", "links"]
-                filter = [filters[int(i)] for i in filter_itens_input]
 
-                Carbon.client.loop.run_until_complete(Carbon.toLocalSave(SOURCE, path_save=path_save_input, filter=filter))
+                credentials = functions.load_credentials()
+
+                if credentials == 0:
+                    error_credentials()
+                
+                else:
+                    banners.carbon_banner_bluured()
+                    print('\033[1mC\033[0mLONAR_\033[1mC\033[0mANAL - SAVE_LOCAL \n')
+                    source_input = str(input(f'> Source Group: t.me/'))
+                    SOURCE = f"t.me/{source_input}" # INSIRA SOMENTE O NOME t.me/Nome
+                    DESTINATION = str(input(f'> Local Save: '))
+                    
+                    panels.filter_itens_panel()
+                    filter_itens_input = list(input('> Insira o numero de cada opcao que deseja filtrar: '))
+                    filters = ["messages", "images", "videos", "audios", "docs", "links"]
+                    filter = [filters[int(i)] for i in filter_itens_input]
+
+                    Carbon.client.loop.run_until_complete(Carbon.toLocalSave(SOURCE, path_save=DESTINATION, filter=filter))
                 
             elif choose_option_save_clone == 3:
-                banners.carbon_banner_bluured()
-                print('Ainda em desenvolvimento')
-                time.sleep(1.0)
-                input('\n...PRESS ANY KEY...')
+
+                credentials = functions.load_credentials()
+
+                if credentials == 0:
+                    error_credentials()
+                    
+                else:
+                    banners.carbon_banner_bluured()
+                    print('Ainda em desenvolvimento')
+                    time.sleep(1.0)
+                    input('\n...PRESS ANY KEY...')
                 
             elif choose_option_save_clone == 0:
                 break
