724456d74f6e4fbafdb24fd7a4e1d6befde755da
Maserati <maserati@localhost.localdomain>
2025-04-06 14:04:03 -0400

    atualizacao

--- system.py
+++ system.py
@@ -6,7 +6,7 @@ def load_log(log:str, log_path='log.txt'):
     try:
         with open(log_path, 'a') as file:
             date_time_local = datetime.now()
-            file.write(f'{date_time_local.strftime("%Y-%m-%d %H:%M:%S")} {log} \n')
+            file.write(f'{date_time_local.strftime("%Y-%m-%d %H:%M:%S")} {log}')
             return 1
     except Exception as err:
         return 0
