1211f53e56fea0aabc1f7447d22f1d1fcc0a2ee4
Maserati <maserati@localhost.localdomain>
2025-04-10 00:15:24 -0400

    Implementacao do modo sem filtro e stickers

--- carbon.py
+++ carbon.py
@@ -69,40 +69,53 @@ class Carbon:
 
             source_entity = await client.get_entity(source)
             async for message in client.iter_messages(source_entity, reverse=True, limit=limit):
-                print(f'\033[1;33m[INFO]\033[0m Content: {message.id} {message.entities}')
-                system.load_log(f'[INFO] Content: {message.id} {message.entities}\n')
+                print(f'\033[1;33m[INFO]\033[0m[{message.date}] METADATA: {message.id} {message.entities}')
+                system.load_log(f'[INFO][{message.date}] msg.ID: {message.id} group.ID: {message.chat_id} {message.out} {message.entities}\n')
                 try:
                     await asyncio.sleep(0.85) # wait
-                    if 'images' in filter and message.photo:
-                        print(f'\033[1;33m[INFO]\033[0m image {message.id}\n')
-                        system.load_log(f'[INFO] image {message.id}\n')
+                    
+                    if len(filter) == 0:
+                        print(f'\033[1;33m[INFO]\033[0m None Type: {message.id}\n')
+                        system.load_log(f'[INFO] {message.id}\n')
                         await client.send_file(destination, message.media, caption=message.text)
 
-                    elif 'videos' in filter and message.video:
-                        print(f'\033[1;33m[INFO]\033[0m video {message.id}\n')
-                        system.load_log(f'[INFO] video {message.id}\n')
-                        await client.send_file(destination, message.media, caption=message.text)
+                    else:
+                        if 'images' in filter and message.photo:
+                            print(f'\033[1;33m[INFO]\033[0m image {message.id}\n')
+                            system.load_log(f'[INFO] image {message.id}\n')
+                            await client.send_file(destination, message.media, caption=message.text)
 
-                    elif 'docs' in filter and message.document:
-                        print(f'\033[1;33m[INFO]\033[0m doc {message.id}\n')
-                        system.load_log(f'[INFO] doc {message.id}\n')
-                        await client.send_file(destination, message.media, caption=message.text)
+                        elif 'videos' in filter and message.video:
+                            print(f'\033[1;33m[INFO]\033[0m video {message.id}\n')
+                            system.load_log(f'[INFO] video {message.id}\n')
+                            await client.send_file(destination, message.media, caption=message.text)
 
-                    elif 'audios' in filter and message.audio:
-                        print(f'\033[1;33m[INFO]\033[0m audio {message.id}\n')
-                        system.load_log(f'[INFO] audio {message.id}\n')
-                        await client.send_file(destination, message.media, caption=message.text)
+                        elif 'docs' in filter and message.document:
+                            print(f'\033[1;33m[INFO]\033[0m doc {message.id}\n')
+                            system.load_log(f'[INFO] doc {message.id}\n')
+                            await client.send_file(destination, message.media, caption=message.text)
 
-                    elif 'links' in filter and message.text:
-                        if 'http' in message.text or 'https' in message.text:
-                            print(f'\033[1;33m[INFO]\033[0m link {message.id}\n')
-                            system.load_log(f'[INFO] link {message.id}\n')
+                        elif 'audios' in filter and message.audio or message.voice:
+                            print(f'\033[1;33m[INFO]\033[0m audio {message.id}\n')
+                            system.load_log(f'[INFO] audio {message.id}\n')
+                            await client.send_file(destination, message.media, caption=message.text)
+
+                        elif 'stickers' in filter and message.sticker:
+                            print(f'\033[1;33m[INFO]\033[0m sticker {message.id}\n')
+                            system.load_log(f'[INFO] sticker {message.id}\n')
+                            await client.send_file(destination, message.media, caption=message.text)
+
+                        elif 'links' in filter and message.text:
+                            if 'http' in message.text or 'https' in message.text:
+                                print(f'\033[1;33m[INFO]\033[0m link {message.id}\n')
+                                system.load_log(f'[INFO] link {message.id}\n')
+                                await client.send_message(destination, message.text)
+
+                        elif 'messages' in filter and message.text:
+                            print(f'\033[1;33m[INFO]\033[0m message {message.id}\n')
+                            system.load_log(f'[INFO] message {message.id}\n')
                             await client.send_message(destination, message.text)
-
-                    elif 'messages' in filter and message.text:
-                        print(f'\033[1;33m[INFO]\033[0m message {message.id}\n')
-                        system.load_log(f'[INFO] message {message.id}\n')
-                        await client.send_message(destination, message.text)
+                        
                 except Exception as e:
                     print(f'\033[1;31m[WARN]\033[0m {message.id} {e}\n')
                     system.load_log(f'[WARN] {message.id} {e}\n')
@@ -151,50 +164,81 @@ class Carbon:
 
             source_entity = await client.get_entity(source)
             async for message in client.iter_messages(source_entity, reverse=True, limit=limit):
-                print(f'\033[1;33m[INFO]\033[0m Content: {message.id} {message.entities}')
-                system.load_log(f'[INFO] Content: {message.id} {message.entities}\n')
+                print(f'\033[1;33m[INFO]\033[0m[{message.date}] METADATA: {message.id} {message.entities}')
+                system.load_log(f'[INFO][{message.date}] msg.ID: {message.id} group.ID: {message.chat_id} {message.out} {message.entities}\n')
                 try:
                     await asyncio.sleep(0.85)
-                    if 'images' in filter and message.photo:
-                        os.makedirs(f'{path_save}/imgs/', exist_ok=True) # if exists
-                        file_path = await message.download_media(file=f'{path_save}/imgs/')
-                        print(f'\033[1;33m[INFO]\033[0m image saved {message.id} {file_path}\n')
-                        system.load_log(f'[INFO] image saved {message.id} {file_path}\n')
+                    if len(filter) == 0:
+                        if message.media:
+                            os.makedirs(f'{path_save}/medias/', exist_ok=True) # if exists
+                            file_path = await message.download_media(file=f'{path_save}/medias/')
+                            print(f'\033[1;33m[INFO]\033[0m media saved {message.id} {file_path}\n')
+                            system.load_log(f'[INFO] media saved {message.id} {file_path}\n')
+                            
+                        elif 'links' in filter and message.text:
+                            if 'http' in message.text or 'https' in message.text:
+                                sender = await client.get_entity(message.sender_id)
+                                with open(f'{path_save}/links.txt', 'a', encoding='utf-8') as f:
+                                    sender_name = sender.first_name if sender.first_name else "Unknown"
+                                    f.write(f"{message.date} : Sender:({message.sender_id}|{sender_name}) Chat:{message.chat_id} : {message.text}\n")
+                                print(f'\033[1;33m[INFO]\033[0m link saved {message.id} {file_path}\n')
+                                system.load_log(f'[INFO] link saved {message.id} {file_path}\n')
 
-                    elif 'videos' in filter and message.video:
-                        os.makedirs(f'{path_save}/videos/', exist_ok=True)
-                        file_path = await message.download_media(file=f'{path_save}/videos/')
-                        print(f'\033[1;33m[INFO]\033[0m video saved {message.id} {file_path}\n')
-                        system.load_log(f'[INFO] video saved {message.id} {file_path}\n')
-
-                    elif 'docs' in filter and message.document:
-                        os.makedirs(f'{path_save}/docs/', exist_ok=True)
-                        file_path = await message.download_media(file=f'{path_save}/docs/')
-                        print(f'\033[1;33m[INFO]\033[0m doc saved {message.id} {file_path}\n')
-                        system.load_log(f'[INFO] doc saved {message.id} {file_path}\n')
-
-                    elif 'audios' in filter and message.audio:
-                        os.makedirs(f'{path_save}/audios/', exist_ok=True)
-                        file_path = await message.download_media(file=f'{path_save}/audio/')
-                        print(f'\033[1;33m[INFO]\033[0m audio saved {message.id} {file_path}\n')
-                        system.load_log(f'[INFO] audio saved {message.id} {file_path}\n')
-
-                    elif 'links' in filter and message.text:
-                        if 'http' in message.text or 'https' in message.text:
+                        elif 'messages' in filter and message.text:
                             sender = await client.get_entity(message.sender_id)
-                            with open(f'{path_save}/links.txt', 'a', encoding='utf-8') as f:
+                            with open(f'{path_save}/messages.txt', 'a', encoding='utf-8') as f:
                                 sender_name = sender.first_name if sender.first_name else "Unknown"
                                 f.write(f"{message.date} : Sender:({message.sender_id}|{sender_name}) Chat:{message.chat_id} : {message.text}\n")
-                            print(f'\033[1;33m[INFO]\033[0m link saved {message.id} {file_path}\n')
-                            system.load_log(f'[INFO] link saved {message.id} {file_path}\n')
+                            print(f'\033[1;33m[INFO]\033[0m message saved {message.id} {file_path}\n')
+                            system.load_log(f'[INFO] message saved {message.id} {file_path}\n')
 
-                    elif 'messages' in filter and message.text:
-                        sender = await client.get_entity(message.sender_id)
-                        with open(f'{path_save}/messages.txt', 'a', encoding='utf-8') as f:
-                            sender_name = sender.first_name if sender.first_name else "Unknown"
-                            f.write(f"{message.date} : Sender:({message.sender_id}|{sender_name}) Chat:{message.chat_id} : {message.text}\n")
-                        print(f'\033[1;33m[INFO]\033[0m message saved {message.id} {file_path}\n')
-                        system.load_log(f'[INFO] message saved {message.id} {file_path}\n')
+                    else:
+                        if 'images' in filter and message.photo:
+                            os.makedirs(f'{path_save}/imgs/', exist_ok=True) # if exists
+                            file_path = await message.download_media(file=f'{path_save}/imgs/')
+                            print(f'\033[1;33m[INFO]\033[0m image saved {message.id} {file_path}\n')
+                            system.load_log(f'[INFO] image saved {message.id} {file_path}\n')
+
+                        elif 'videos' in filter and message.video:
+                            os.makedirs(f'{path_save}/videos/', exist_ok=True)
+                            file_path = await message.download_media(file=f'{path_save}/videos/')
+                            print(f'\033[1;33m[INFO]\033[0m video saved {message.id} {file_path}\n')
+                            system.load_log(f'[INFO] video saved {message.id} {file_path}\n')
+
+                        elif 'docs' in filter and message.document:
+                            os.makedirs(f'{path_save}/docs/', exist_ok=True)
+                            file_path = await message.download_media(file=f'{path_save}/docs/')
+                            print(f'\033[1;33m[INFO]\033[0m doc saved {message.id} {file_path}\n')
+                            system.load_log(f'[INFO] doc saved {message.id} {file_path}\n')
+
+                        elif 'audios' in filter and message.audio:
+                            os.makedirs(f'{path_save}/audios/', exist_ok=True)
+                            file_path = await message.download_media(file=f'{path_save}/audio/')
+                            print(f'\033[1;33m[INFO]\033[0m audio saved {message.id} {file_path}\n')
+                            system.load_log(f'[INFO] audio saved {message.id} {file_path}\n')
+                        
+                        elif 'stickers' in filter and message.sticker:
+                            os.makedirs(f'{path_save}/stickers/', exist_ok=True)
+                            file_path = await message.download_media(file=f'{path_save}/stickers/')
+                            print(f'\033[1;33m[INFO]\033[0m sticker saved {message.id} {file_path}\n')
+                            system.load_log(f'[INFO] sticker saved {message.id} {file_path}\n')
+
+                        elif 'links' in filter and message.text:
+                            if 'http' in message.text or 'https' in message.text:
+                                sender = await client.get_entity(message.sender_id)
+                                with open(f'{path_save}/links.txt', 'a', encoding='utf-8') as f:
+                                    sender_name = sender.first_name if sender.first_name else "Unknown"
+                                    f.write(f"{message.date} : Sender:({message.sender_id}|{sender_name}) Chat:{message.chat_id} : {message.text}\n")
+                                print(f'\033[1;33m[INFO]\033[0m link saved {message.id} {file_path}\n')
+                                system.load_log(f'[INFO] link saved {message.id} {file_path}\n')
+
+                        elif 'messages' in filter and message.text:
+                            sender = await client.get_entity(message.sender_id)
+                            with open(f'{path_save}/messages.txt', 'a', encoding='utf-8') as f:
+                                sender_name = sender.first_name if sender.first_name else "Unknown"
+                                f.write(f"{message.date} : Sender:({message.sender_id}|{sender_name}) Chat:{message.chat_id} : {message.text}\n")
+                            print(f'\033[1;33m[INFO]\033[0m message saved {message.id} {file_path}\n')
+                            system.load_log(f'[INFO] message saved {message.id} {file_path}\n')
                 except Exception as e:
                     print(f'\033[1;31m[WARN]\033[0m {message.id} {e}')
                     system.load_log(f'[WARN] {message.id} {e}\n')
